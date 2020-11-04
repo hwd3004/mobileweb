86dba89c68cfe8b62029d5122090f96d08386596
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-11-04 11:10:16 +0900

    3. npm i shortid

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ npm i moment
 ---
 
 # 2. 컴포넌트에서 리덕스 코드들 디스트럭쳐링
+
+---
+
+# 3. npm i shortid
+
+글 새로 작성시 생성된 url로 바로 이동하기 위해 shortid 설치
--- src/FreeBoardWrite.js
+++ src/FreeBoardWrite.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-
 import { connect } from "react-redux";
+import { authService, dbService, storageService } from "./fbase";
+import moment from "moment";
 
 const FreeBoardWrite = ({ reducerLog }) => {
   const [text, setText] = useState("");
@@ -8,6 +9,34 @@ const FreeBoardWrite = ({ reducerLog }) => {
 
   const onSumbit = async (event) => {
     event.preventDefault();
+
+    let fileUrl = "";
+
+    if (attachment !== "") {
+      const attachmentRef = storageService
+        .ref()
+        .child(
+          `FreeBoard/${moment().format("YYMMDD")}/${moment().format("HHmmss")}`
+        );
+
+      const response = await attachmentRef.putString(attachment, "data_url");
+
+      fileUrl = await response.ref.getDownloadURL();
+
+      const textObj = {
+        category: "FreeBoard",
+        text,
+        createdAt: moment().format("YYYY-MM-DD HH:mm:ss"),
+        creatorUid: authService.currentUser.uid,
+        email: authService.currentUser.email,
+        upVote: 0,
+        comment: [],
+        views: 0,
+        fileUrl,
+      };
+
+      
+    }
   };
   return <div id="FreeBoard"></div>;
 };
